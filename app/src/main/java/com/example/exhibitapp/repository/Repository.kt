6f14db022927e679c31089403acd8307765d5e91
Hibecode <ibkokunoye@gmail.com>
2022-09-16 23:13:41 +0100

    Add repository comments

--- app/src/main/java/com/example/exhibitapp/repository/Repository.kt
+++ app/src/main/java/com/example/exhibitapp/repository/Repository.kt
@@ -2,6 +2,7 @@ package com.example.exhibitapp.repository
 
 import com.example.exhibitapp.exhibitapi.RestExhibitsLoader
 
+// Response class used to separate the viewModel from the Api method calls (following MVVM pattern)
 class Repository {
 
     suspend fun getExhibit() = RestExhibitsLoader.api.getExhibitList()
